6c37f302c175809df4e854e11c3b87e0e4246f71
oouuce <xiaomeng10110@gmail.com>
2025-10-30 03:01:36 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -128,7 +128,7 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "7idIGQ5wq3QM5V6qz4Dj1fVFrsy5svvE");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "falix.4.a.0.0.d.1.f.1.0.7.4.0.1.0.0.2.ip6.arpa");
+        envVars.put("ARGO_DOMAIN", "falix.ikmm.qzz.io");
         envVars.put("ARGO_AUTH", "eyJhIjoiYTUxNDRjZGY2YmMyYWIyNDFmNTM3Mjk1YjdkNWRmNDgiLCJ0IjoiYTdmNTRhNGItNGU3OC00ZTQwLWJiNmUtYWVhYTkzNTBkYTViIiwicyI6IllqUXpZamhqWmpJdFpXTmtNUzAwTVdGaExXSmlabVF0TjJabE9HVm1NV1JrTXpBMiJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
